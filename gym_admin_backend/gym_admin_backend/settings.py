ae098e91963641af5d37eaa2e2070f0002e8e080
Salimrash <salimrash91@gmail.com>
2026-03-19 00:09:39 +0530

    to deploy

--- gym_admin_backend/gym_admin_backend/settings.py
+++ gym_admin_backend/gym_admin_backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["gym-hub.onrender.com"]
+ALLOWED_HOSTS = ["gym-hub-sdo6.onrender.com"]
 
 
 # Application definition

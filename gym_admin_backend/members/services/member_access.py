cb132c256bdbe8f56af5f7459182f4989c4065a2
Salimrash <salimrash91@gmail.com>
2026-03-21 18:42:44 +0530

    Brevo integration for email sending for production

--- gym_admin_backend/members/services/member_access.py
+++ gym_admin_backend/members/services/member_access.py
@@ -1,11 +1,14 @@
 from django.conf import settings
 from django.core.mail import send_mail
+from dotenv import load_dotenv
+import os
 
 from members.models import Member
 
 
 def send_member_link(member):
-    link = f"http://localhost:5173/memberaccess/{member.access_token}"
+    link_header=os.getenv("CORS_ALLOWED_ORIGINS")
+    link = f"{link_header}/{member.access_token}"
     send_mail(
         subject='Your Gym Access Link',
         message=f"Use this link to mark attendance and for Payments: {link}",

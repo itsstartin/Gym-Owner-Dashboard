faaaad2e55c868408e8829f8c2554e5891d6fe1e
Salimrash <salimrash91@gmail.com>
2026-03-21 23:10:29 +0530

    Brevo api email setup completed + added fix for routing in production

--- gym_admin_backend/members/services/member_access.py
+++ gym_admin_backend/members/services/member_access.py
@@ -28,16 +28,25 @@ def send_member_link(member):
 
 
 def send_mail_overdue_member(data):
-    member = Member.objects.get(id=data["id"])
-    exp_date = data["membership_end_date"]
-    overdue_amount=data["overdue_amount"]
-    overdue_days=data["overdue_days"]
     try:
-        send_mail(
-            subject='Gym Payment Overdue Notification',
-            message=f"Your membership expired in {exp_date} and have pending payments of {overdue_amount} for {overdue_days} days",
-            from_email=settings.DEFAULT_FROM_EMAIL,
-            recipient_list=[member.email],
-        )
+        member = Member.objects.get(id=data["id"])
+        exp_date = data["membership_end_date"]
+        overdue_amount=data["overdue_amount"]
+        overdue_days=data["overdue_days"]
+        url = "https://api.brevo.com/v3/smtp/email"
+        headers = {
+        "accept":"application/json",
+        "api-key":os.getenv("BREVO_API_KEY"),
+        "content-type":"application/json"
+        }
+        data_json = {
+            "sender":{"email":os.getenv("EMAIL_SENDER")},
+            "to":[{"email":member.email}],
+            "subject":'Gym Payment Overdue Notification',
+            "textContent":f"Your membership expired in {exp_date} and have pending payments of {overdue_amount} for {overdue_days} days",
+        }
+        response = requests.post(url, json=data_json, headers=headers)
+        print("STATUS:",response.status_code)
+        print("RESPONSE:",response.text)
     except Exception as e:
         print("Email failed:",e)
